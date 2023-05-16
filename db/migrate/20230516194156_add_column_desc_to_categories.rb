8cabd0953ac3d88a125e6ee42d7e315a929b270f
Fernando Sosa <sosafernando936@gmail.com>
2023-05-16 16:44:22 -0300

    add column description to categories

---
+++ db/migrate/20230516194156_add_column_desc_to_categories.rb
@@ -0,0 +1,6 @@
+class AddColumnDescToCategories < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :categories, :descripcion
+    add_column :categories, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_192425) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_194156) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "exams", force: :cascade do |t|
