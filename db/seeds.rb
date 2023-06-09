da2018fce58d9138e5800fddb6c8aad6d234fe98
ignacioDias <ignadiasg@gmail.com>
2023-06-09 11:35:37 -0300

    preguntas

---
+++ db/seeds.rb
@@ -0,0 +1,85 @@
+Question.destroy_all
+
+
+Question.create(question: "¿Dónde nació Lionel Messi?", answer: "Rosario", wrongAnswer1: "C.A. Buenos Aires", wrongAnswer3: "Córdoba", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "La Plata")
+
+Question.create(question: "¿Cuál es el clásico de Rosario Central?", answer: "Newell's", wrongAnswer1: "Colón de Santa Fe", wrongAnswer3: "Unión de Santa Fe", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Instituto A.C.C.")
+
+Question.create(question: "¿Quién es ídolo de Boca entre las opciones de abajo?", answer: "Riquelme", wrongAnswer1: "Diego Ceballos", wrongAnswer3: "Ángel Labruna", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Diego Milito")
+
+Question.create(question: "¿De qué equipo es Marcelo Gallardo?", answer: "River Plate", wrongAnswer1: "Boca Juniors", wrongAnswer3: "Independiente", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Racing")
+
+Question.create(question: "¿Cuál es el clásico principal de Boca Juniors?", answer: "River Plate", wrongAnswer1: "Racing", wrongAnswer3: "San Lorenzo", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Independiente")
+
+Question.create(question: "¿Cómo se llama el estadio de River Plate?", answer: "El Monumental", wrongAnswer1: "La Bombonera", wrongAnswer3: "El Cilindro", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "El Cementerio de Elefantes")
+
+Question.create(question: "¿Quién fue el 10 y referente de la selección en el mundial de 1986?", answer: "Diego Armando Maradona", wrongAnswer1: "Mario Alberto Kempes", wrongAnswer3: "Claudo Caniggia", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Gabriel Batistuta")
+
+Question.create(question: "¿Cómo se le llama a la Selección de Rugby?", answer: "Los Pumas", wrongAnswer1: "Las Panteras", wrongAnswer3: "Los Leones", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Los Tigres")
+
+Question.create(question: "¿Cómo se le dice a la Selección de Hockey femenina?", answer: "Las Leonas", wrongAnswer1: "Las Panteras", wrongAnswer3: "Las Pumas", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Las Tigresas")
+
+Question.create(question: "¿Quíen fue el referente de la Generación Dorada del Basket Argentino?", answer: "Manuel Ginóbili", wrongAnswer1: "Luis Scola", wrongAnswer3: "Juan Martín Del Potro", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Paulo Dybala")
+
+Question.create(question: "¿Quién hizo goles en las finales jugadas contra Brasil, Italia y Francia entre 2022 y 2023?", answer: "Ángel Di María", wrongAnswer1: "Lionel Messi", wrongAnswer3: "Julián Álvarez", pointQuestion: 10, exam_id: nil, level_id: 13, wrongAnswer2: "Lautaro Martínez")
+
+
+Question.create(question: "¿En qué equipo de Inglaterra es ídolo el Kun Agüero?", answer: "Manchester City", wrongAnswer1: "Manchester United", wrongAnswer3: "Chelsea", pointQuestion: 15, exam_id: nil, level_id: 14, wrongAnswer2: "Liverpool")
+
+Question.create(question: "¿Que equipo tiene más Copas Libertadores en todo el continente?", answer: "Independiente", wrongAnswer1: "Boca Juniors", wrongAnswer3: "Estudiantes de La Plata", pointQuestion: 15, exam_id: nil, level_id: 14, wrongAnswer2: "River Plate")
+
+Question.create(question: "¿Quién es el argentino con más mundiales?", answer: "Daniel Passarella", wrongAnswer1: "Diego Armando Maradona", wrongAnswer3: "Mario Alberto Kempes", pointQuestion: 15, exam_id: nil, level_id: 14, wrongAnswer2: "Claudio Caniggia")
+
+Question.create(question: "¿En qué equipo de Inglaterra es ídolo el Kun Agüero?", answer: "Manchester City", wrongAnswer1: "Manchester United", wrongAnswer3: "Chelsea", pointQuestion: 15, exam_id: nil, level_id: 14, wrongAnswer2: "Liverpool")
+
+Question.create(question: "¿En qué equipo de Inglaterra es ídolo el Kun Agüero?", answer: "Manchester City", wrongAnswer1: "Manchester United", wrongAnswer3: "Chelsea", pointQuestion: 15, exam_id: nil, level_id: 14, wrongAnswer2: "Liverpool")
+
+Question.create(question: "¿En dónde se jugó la vuelta de la final de Libertadores entre River y Boca?", answer: "El Santiago Bernabeu", wrongAnswer1: "La Bombonera", wrongAnswer3: "El Monumental", pointQuestion: 15, exam_id: nil, level_id: 14, wrongAnswer2: "El Maracaná")
+
+Question.create(question: "¿En qué equipo de Inglaterra es ídolo el Kun Agüero?", answer: "Manchester City", wrongAnswer1: "Manchester United", wrongAnswer3: "Chelsea", pointQuestion: 15, exam_id: nil, level_id: 14, wrongAnswer2: "Liverpool")
+
+Question.create(question: "¿Quién es el máximo goleador de la historia de River Plate?", answer: "Ángel Labruna", wrongAnswer1: "Oscar Más", wrongAnswer3: "Bernabé Ferreyra", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "Gonzalo Higuaín")
+
+Question.create(question: "¿Quién es el máximo goleador de la historia de Boca Juniors?", answer: "Martín Palermo", wrongAnswer1: "Juan Román Riquelme", wrongAnswer3: "Carlos Tévez", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "Roberto Cherro")
+
+Question.create(question: "¿Qué argentino fue la venta más cara del fútbol local?", answer: "Lautaro Martínez", wrongAnswer1: "Sergio Agüero", wrongAnswer3: "Lionel Messi", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "Diego Armando Maradona")
+
+Question.create(question: "¿En qué año ganó se jugó la final entre Estudiantes de La Plata y Barcelona?", answer: "2009", wrongAnswer1: "2006", wrongAnswer3: "2008", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "2007")
+
+
+Question.create(question: "¿En qué año se fundó River Plate?", answer: "1901", wrongAnswer1: "1899", wrongAnswer3: "1903", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "1904")
+
+Question.create(question: "¿En qué año Boca Juniors ganó su primera Libertadores?", answer: "1977", wrongAnswer1: "1980", wrongAnswer3: "1992", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "1989")
+
+Question.create(question: "¿En qué año Independiente ganó su última Libertadores?", answer: "1975", wrongAnswer1: "1991", wrongAnswer3: "1983", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "1984")
+
+Question.create(question: "¿En qué año ganó su única Intercontinental Racing Club?", answer: "1967", wrongAnswer1: "1993", wrongAnswer3: "1973", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "1984")
+Question.create(question: "¿En qué año debutó Lionel Messi?", answer: "1967", wrongAnswer1: "1993", wrongAnswer3: "1973", pointQuestion: 20, exam_id: nil, level_id: 15, wrongAnswer2: "1984")
+
+Question.create(question: "¿Quién cantaba en Soda Stereo?", answer: "Gustavo Cerati", wrongAnswer1: "Fito Páez", wrongAnswer3: "Luis Spinetta", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "Charly García")
+Question.create(question: "¿Quién canta De Musica Ligera?", answer: "Soda Stereo", wrongAnswer1: "Fito Páez", wrongAnswer3: "Divididos", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "La Beriso")
+Question.create(question: "¿Quién tiene la BZRP Session más escuchada?", answer: "Shakira", wrongAnswer1: "Quevedo", wrongAnswer3: "Nathy Peluso", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "Nicki Nicole")
+Question.create(question: "¿Qué cantante llama a sus fans \"Los Leones con FLow?\"", answer: "Paulo Londra", wrongAnswer1: "Duki", wrongAnswer3: "Trueno", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "Khea")
+Question.create(question: "¿Qué cantante es conocido como la cara del trap argentino?", answer: "Duki", wrongAnswer1: "Paulo Londra", wrongAnswer3: "Ysy A", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "Neo Pistea")
+Question.create(question: "¿De qué equipo era Pepe Argentino de Casados con Hijos?", answer: "Racing", wrongAnswer1: "River Plate", wrongAnswer3: "Boca Juniors", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "No le gustaba el fútbol")
+Question.create(question: "¿Quién es el actor principal de Argentina:1985?", answer: "Ricardo Darín", wrongAnswer1: "Guillermo Francella", wrongAnswer3: "Leonardo Sbaraglia", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "Miguel Ángel Solá")
+Question.create(question: "¿Qué artista argentina protagoniza Violetta?", answer: "Martina Stoessel", wrongAnswer1: "Nicki Nicole", wrongAnswer3: "María Becerra", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "Lali Espósito")
+Question.create(question: "¿Qué artista argentina protagoniza el Gambito de Dama / Queen's Gambit?", answer: "Anya Taylor-Joy", wrongAnswer1: "Lali Espósito", wrongAnswer3: "Belén Blanco", pointQuestion: 5, exam_id: nil, level_id: 1, wrongAnswer2: "Thelma Fardín")
+
+Question.create(question: "¿Qué instrumento era el principal de Fito Páez?", answer: "Teclados", wrongAnswer1: "Cuerdas", wrongAnswer3: "Ninguno", pointQuestion: 10, exam_id: nil, level_id: 2, wrongAnswer2: "Teclados y cuerdas")
+Question.create(question: "¿De dónde vienen Duki, Paulo Londra y demás cantantes urbanos?", answer: "El Quinto Escalón", wrongAnswer1: "La Voz Argentina", wrongAnswer3: "FMS Argentina", pointQuestion: 10, exam_id: nil, level_id: 2, wrongAnswer2: "Ningún lugar en común")
+Question.create(question: "¿En qué estadio argentino grabó su disco AC/DC?", answer: "El Monumental", wrongAnswer1: "La Bombonera", wrongAnswer3: "Vélez", pointQuestion: 10, exam_id: nil, level_id: 2, wrongAnswer2: "El Cilindro")
+Question.create(question: "¿Con qué artista no grabó un tema Fito Páez?", answer: "Gustavo Cerati", wrongAnswer1: "Luis Spinetta", wrongAnswer3: "Charly García", pointQuestion: 10, exam_id: nil, level_id: 2, wrongAnswer2: "Grabó con todos")
+Question.create(question: "¿Cuál es el disco más vendido de la historia del país?", answer: "El amor después del amor", wrongAnswer1: "SEP7IMO DÍA", wrongAnswer3: "Ahí vamos", pointQuestion: 10, exam_id: nil, level_id: 2, wrongAnswer2: "Fuerza Natural")
+
+Question.create(question: "¿En qué año murió Spinetta?", answer: "2012", wrongAnswer1: "2014", wrongAnswer3: "2008", pointQuestion: 15, exam_id: nil, level_id: 3, wrongAnswer2: "2001")
+Question.create(question: "¿En qué año se estrenó la película Metegol?", answer: "2013", wrongAnswer1: "2014", wrongAnswer3: "2016", pointQuestion: 15, exam_id: nil, level_id: 3, wrongAnswer2: "2012")
+Question.create(question: "¿En qué año se estrenó El Secreto de sus Ojos?", answer: "2009", wrongAnswer1: "2007", wrongAnswer3: "2012", pointQuestion: 15, exam_id: nil, level_id: 3, wrongAnswer2: "2011")
+Question.create(question: "¿Cuántos Oscars ganó Ricardo Darín?", answer: "1", wrongAnswer1: "0", wrongAnswer3: "2", pointQuestion: 15, exam_id: nil, level_id: 3, wrongAnswer2: "3")
+Question.create(question: "¿En qué año murió Cerati?", answer: "2014", wrongAnswer1: "2013", wrongAnswer3: "2009", pointQuestion: 15, exam_id: nil, level_id: 3, wrongAnswer2: "2011")
+
+
+
+Question.create(question: "¿?", answer: "", wrongAnswer1: "", wrongAnswer3: "", pointQuestion: 5, exam_id: nil, level_id: 4, wrongAnswer2: "")
+Question.create(question: "¿?", answer: "", wrongAnswer1: "", wrongAnswer3: "", pointQuestion: 10, exam_id: nil, level_id: 5, wrongAnswer2: "")
+Question.create(question: "¿?", answer: "", wrongAnswer1: "", wrongAnswer3: "", pointQuestion: 15, exam_id: nil, level_id: 6, wrongAnswer2: "")
