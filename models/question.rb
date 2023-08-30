5ea21e9df6c00e2215b7349d5168ee16707dc546
Fernando Sosa <sosafernando936@gmail.com>
2023-08-30 15:45:28 -0300

    add test Questions

--- models/question.rb
+++ models/question.rb
@@ -3,4 +3,16 @@ class Question < ActiveRecord::Base
   belongs_to :level
   has_many :record_questions
   has_many :records, through: :record_questions
+
+  validates :pointQuestion, presence: true
+  validate :point_question_positive
+  validates :question, presence: true
+
+  private
+
+  def point_question_positive
+    if pointQuestion.to_i <= 0
+      errors.add(:pointQuestion, "must be a positive number")
+    end
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,37 +5,23 @@ RSpec.describe Question do
   describe 'validations' do
     let(:question) { Question.new }
 
-    it 'requires a username' do
-      user.username = ''
-      expect(user.valid?).to be_falsey
-      expect(user.errors[:username]).to include("can't be blank")
+    it 'requires a question' do
+      question.question = ''
+      expect(question.valid?).to be_falsey
+      expect(question.errors[:question]).to include("can't be blank")
     end
 
-    it 'requires a unique email' do
-      existing_user = User.create(username: 'existing_user', email: 'test@example.com', password: 'password')
-      user.email = existing_user.email
-      expect(user.valid?).to be_falsey
-      expect(user.errors[:email]).to include("has already been taken")
+    it 'requires positive points' do
+      question.pointQuestion = -10
+      expect(question.valid?).to be_falsey
+      expect(question.errors[:pointQuestion]).to include("must be a positive number")
     end
 
-    it 'requires a valid email format' do
-      user.email = 'invalid_email'
-      expect(user.valid?).to be_falsey
-      expect(user.errors[:email]).to include("is not a valid email address")
-    end
-
-    it 'requires a password with minimum length' do
-      user.password = '1234'
-      expect(user.valid?).to be_falsey
-      expect(user.errors[:password]).to include("is too short (minimum is 5 characters)")
-    end
-
-    it 'is valid with a username, email, and password' do
-      user.username = 'test_user'
-      user.email = 'test2@example.com'
-      user.password = 'password'
-      expect(user.valid?).to be_truthy
+    it 'requires differents answers' do
+      question.answer = 'Buenos Aires'
+      question.wrongAnswer1 = 'buenos aires'
+      expect(question.valid?).to be_falsey
+      expect(question.errors[:answer]).to include("must be different answers")
     end
   end
 end
-   
