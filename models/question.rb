d70a3c84cd28844fc966508ac96ef1b74eeb67bb
Fernando Sosa <sosafernando936@gmail.com>
2023-09-02 17:15:31 -0300

    Test question finished

--- models/question.rb
+++ models/question.rb
@@ -7,12 +7,23 @@ class Question < ActiveRecord::Base
   validates :pointQuestion, presence: true
   validate :point_question_positive
   validates :question, presence: true
+  validate :unique_answer
 
   private
 
   def point_question_positive
     if pointQuestion.to_i <= 0
       errors.add(:pointQuestion, "must be a positive number")
+    end
+  end
+
+  def unique_answer
+    array_answers = [answer.to_s.downcase, wrongAnswer1.to_s.downcase, wrongAnswer2.to_s.downcase, wrongAnswer3.to_s.downcase]
+    for ans in array_answers do
+        array_answers.shift
+        if array_answers.include?(ans)
+          errors.add(:answer, "must be different answers")
+        end
     end
   end
 end
