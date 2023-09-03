59e316a8fb9e42c0cbc156baa336b33908cc2709
ignacioDias <ignadiasg@gmail.com>
2023-09-03 18:16:38 -0300

    intento de test

--- models/category.rb
+++ models/category.rb
@@ -1,3 +1,16 @@
 class Category < ActiveRecord::Base
   has_many :levels
+
+  validate :must_have_at_least_one_level
+  validate :valid_name
+
+  def valid_name
+    if name = ""
+      errors.add(:name, "Empty name")
+
+  def must_have_at_least_one_level
+    if levels.empty?
+      errors.add(:levels|, "Category must have at least one associated level")
+    end
+  end
 end
