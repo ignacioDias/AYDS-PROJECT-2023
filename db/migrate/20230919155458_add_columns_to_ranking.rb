86e94ed7ca6061a71e254c5054688eeb840e1a2d
Fernando Sosa <sosafernando936@gmail.com>
2023-09-19 13:00:23 -0300

    add columns to rankings

---
+++ db/migrate/20230919155458_add_columns_to_ranking.rb
@@ -0,0 +1,6 @@
+class AddColumnsToRanking < ActiveRecord::Migration[7.0]
+  def change
+    add_column :rankings, :username, :string
+    add_column :rankings, :points, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_17_233307) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_155458) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -69,6 +69,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_17_233307) do
   create_table "rankings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.integer "points"
   end
 
   create_table "record_exams", force: :cascade do |t|
