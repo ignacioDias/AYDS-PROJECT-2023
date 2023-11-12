424692ccddb2c3e3f0487f3cd4f854ae308c7a04
Fernando Sosa <sosafernando936@gmail.com>
2023-11-12 20:29:07 -0300

    V2.0

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@
 
 # config/environment.rb
 # db_options = YAML.safe_load(File.read(File.expand_path('database.yml', __dir__)))
-db_options = YAML.safe_load(File.read('./config/database.yml'))
+db_options = YAML.load(File.read('./config/database.yml'))
 environment = ENV['APP_ENV'] || 'development'
 ActiveRecord::Base.establish_connection(db_options[environment])
